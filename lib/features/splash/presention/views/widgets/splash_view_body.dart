ea0fd3e0e4dd0517d61f3fe1e3b0627ac8918e9d
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-04-09 03:27:47 +0200

    feat: implement SplashView and add SplashViewBody with SVG assets

---
+++ lib/features/splash/presention/views/widgets/splash_view_body.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            SvgPicture.asset(Assets.imagesPlant),
+          ],
+        ),
+        SvgPicture.asset(Assets.imagesLogo),
+        SvgPicture.asset(
+          Assets.imagesSplashBottom,
+          fit: BoxFit.fill,
+        ),
+      ],
+    );
+  }
+}
