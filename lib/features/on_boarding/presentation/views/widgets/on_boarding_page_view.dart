09a885b7299879ff8ed6f8559b1776698d12d4a3
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-04-10 00:30:16 +0200

    feat: enhance OnBoardingPageView and PageViewItem layout for improved readability

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
@@ -10,17 +10,35 @@ class OnBoardingPageView extends StatelessWidget {
     return PageView(
       children: const [
         PageViewItem(
-            image: Assets.imagesPageViewItem1Image,
-            backroundImage: Assets.imagesPageViewItem1BackgroundImage,
-            subtitle:
-                'اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.',
-            title: Row(
-              children: [
-                Text('مرحبًا بك في '),
-                Text('Fruit'),
-                Text('HUB'),
-              ],
-            ))
+          image: Assets.imagesPageViewItem1Image,
+          backroundImage: Assets.imagesPageViewItem1BackgroundImage,
+          subtitle:
+              'اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.',
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('مرحبًا بك في '),
+              Text('Fruit'),
+              Text('HUB'),
+            ],
+          ),
+        ),
+        PageViewItem(
+          image: Assets.imagesPageViewItem2Image,
+          backroundImage: Assets.imagesPageViewItem2BackgroundImage,
+          subtitle:
+              'نقدم لك أفضل الفواكه المختارة بعناية. اطلع على التفاصيل والصور والتقييمات لتتأكد من اختيار الفاكهة المثالية',
+          title: Text(
+            'ابحث وتسوق',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xFF0C0D0D),
+              fontSize: 23,
+              fontFamily: 'Cairo',
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
       ],
     );
   }
