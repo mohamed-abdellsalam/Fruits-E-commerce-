717f976f1d09c788e772aeebee0b74bd7f23a23d
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-04-17 18:45:42 +0200

    finish sign in with email and password

--- lib/features/auth/presentation/views/widgets/sign_in_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_view_body_bloc_consumer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/helper_functions/build_error_bar.dart';
 import 'package:fruits_hub/core/widgets/custom_modal_progress_hud.dart';
 import 'package:fruits_hub/features/auth/presentation/cubits/signin_cubits/signin_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/signin_view_body.dart';
@@ -12,7 +13,12 @@ class SignInViewBodyBlocConsumer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<SigninCubit, SigninState>(
-      listener: (context, state) {},
+      listener: (context, state) {
+        if (state is SigninSuccess) {
+        } else if (state is SigninFailure) {
+          buildErrorBar(context, state.message);
+        }
+      },
       builder: (context, state) {
         return CustomProgressHud(
           isLoading: state is SigninLoading ? true : false,
