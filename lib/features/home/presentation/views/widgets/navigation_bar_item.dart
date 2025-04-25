12922b76a5449ceee1512e22250a3578b02e4dee
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-04-25 19:22:48 +0300

    feat: implement CustomBottomNavigationBar and integrate it into HomeView

---
+++ lib/features/home/presentation/views/widgets/navigation_bar_item.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/home/domain/entites/bottom_navigation_bar_entity.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/active_item.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/in_active_item.dart';
+
+class NavigtionBarItem extends StatelessWidget {
+  const NavigtionBarItem(
+      {super.key,
+      required this.isActive,
+      required this.bottomNavigationBarEntity});
+  final bool isActive;
+  final BottomNavigationBarEntity bottomNavigationBarEntity;
+  @override
+  Widget build(BuildContext context) {
+    return isActive
+        ? const ActiveItem()
+        : InActiveItem(
+            image: bottomNavigationBarEntity.inActiveImage,
+          );
+  }
+}
