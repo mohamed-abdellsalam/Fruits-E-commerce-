0cbfdce20813ed7ba08d4c74b11297b0329ce8fa
Mohamed Abdelsalam <149884589+Mohamed3bsalam1@users.noreply.github.com>
2025-07-19 21:32:10 +0300

    feat: add ProductEntity and ReviewEntity models; implement ProductModel and ReviewModel with serialization methods

---
+++ lib/core/entities/product_entity.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:fruits_hub/core/entities/review_entity_model.dart';
+
+class ProductEntity {
+  final String name;
+  final String code;
+  final String description;
+  final double price;
+  final File image;
+  final bool isFeatured;
+  String? imageUrl;
+  final int expirationMonths;
+  final bool isOrganic;
+  final int numberOfcalories;
+  final int unitAmount;
+  final num avgRating = 0;
+  final int ratingCount = 0;
+  final List<ReviewEntity> reviews;
+  ProductEntity({
+    required this.reviews,
+    required this.name,
+    required this.code,
+    required this.description,
+    required this.price,
+    required this.image,
+    required this.isFeatured,
+    required this.expirationMonths,
+    required this.numberOfcalories,
+    required this.unitAmount,
+    this.isOrganic = false,
+    this.imageUrl,
+  });
+}
---
+++ lib/core/models/product_model.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+import 'package:fruits_hub/core/entities/product_entity.dart';
+import 'package:fruits_hub/core/models/review_model.dart';
+
+class ProductModel {
+  final String name;
+  final String code;
+  final String description;
+  final double price;
+  final File image;
+  final bool isFeatured;
+  String? imageUrl;
+  final int expirationMonths;
+  final bool isOrganic;
+  final int numberOfcalories;
+  final int unitAmount;
+  final num avgRating = 0;
+  final int ratingCount = 0;
+  final List<ReviewModel> reviews;
+
+  ProductModel({
+    required this.reviews,
+    required this.expirationMonths,
+    required this.numberOfcalories,
+    required this.unitAmount,
+    required this.name,
+    required this.code,
+    required this.description,
+    required this.price,
+    required this.image,
+    required this.isFeatured,
+    this.isOrganic = false,
+    this.imageUrl,
+  });
+
+  factory ProductModel.fromEntity(
+    ProductEntity addProductInputEntity,
+  ) {
+    return ProductModel(
+      expirationMonths: addProductInputEntity.expirationMonths,
+      numberOfcalories: addProductInputEntity.numberOfcalories,
+      name: addProductInputEntity.name,
+      code: addProductInputEntity.code,
+      description: addProductInputEntity.description,
+      price: addProductInputEntity.price,
+      image: addProductInputEntity.image,
+      isFeatured: addProductInputEntity.isFeatured,
+      unitAmount: addProductInputEntity.unitAmount,
+      isOrganic: addProductInputEntity.isOrganic,
+      reviews: addProductInputEntity.reviews
+          .map((e) => ReviewModel.fromEntity(e))
+          .toList(),
+    );
+  }
+
+  toJson() {
+    return {
+      'name': name,
+      'code': code,
+      'description': description,
+      'price': price,
+      'imageUrl': imageUrl,
+      'isFeatured': isFeatured,
+      'expirationMonths': expirationMonths,
+      'numberOfcalories': numberOfcalories,
+      'unitAmount': unitAmount,
+      'isOrganic': isOrganic,
+      'reviews': reviews.map((e) => e.toJson()).toList(),
+    };
+  }
+}
