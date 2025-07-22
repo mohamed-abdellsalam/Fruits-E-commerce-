df168d587d5dc69add0d50b417e7e29003049876
Mohamed Abdelsalam <149884589+Mohamed3bsalam1@users.noreply.github.com>
2025-07-22 09:27:02 +0300

    feat: update FruitItem to handle null imageUrl; add CustomNetworkImage widget for network image loading

--- lib/core/helper/get_dummy_products.dart
+++ lib/core/helper/get_dummy_products.dart
@@ -13,8 +13,7 @@ ProductEntity getDummyProduct() {
     unitAmount: 1,
     sellingCount: 100,
     isOrganic: true,
-    imageUrl:
-        'https://hips.hearstapps.com/hmg-prod/images/red-apple-cortland-royalty-free-image-1659451218.jpg?crop=0.858xw:0.858xh;0.0691xw,0.0743xh&resize=980:*',
+    imageUrl: null,
   );
 }
 
