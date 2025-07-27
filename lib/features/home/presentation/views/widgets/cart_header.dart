4e21b25275609869ea9aff23bbff3ac72f560e7e
Mohamed Abdelsalam <149884589+Mohamed3bsalam1@users.noreply.github.com>
2025-07-27 14:58:49 +0300

    feat: update CartHeader to display dynamic cart item count using CartCubit

--- lib/features/home/presentation/views/widgets/cart_header.dart
+++ lib/features/home/presentation/views/widgets/cart_header.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:fruits_hub/features/home/presentation/cubit/cart_cubit/cart_cubit.dart';
 
 class CartHeader extends StatelessWidget {
   const CartHeader({super.key});
@@ -12,7 +14,7 @@ class CartHeader extends StatelessWidget {
       decoration: const BoxDecoration(color: Color(0xFFEBF9F1)),
       child: Center(
         child: Text(
-          'لديك 3 منتجات في سله التسوق',
+          'لديك ${context.watch<CartCubit>().cartEntity.carItems.length} منتجات في سله التسوق',
           style: TextStyles.regular13(context).copyWith(
             color: const Color(0xFF1B5E37) /* Green1-900 */,
           ),
